3389acde07c9ad18965e04e0ee168bd1929bee91
Andy Jack <github@veracity.ca>
2015-05-28 17:01:59 -0400

    Start cryptswap in firstboot

--- cthost1-firstboot.sh
+++ cthost1-firstboot.sh
@@ -10,3 +10,10 @@ echo "UUID=$UUID / ext4 defaults 0 0" >> /etc/fstab
 eval $(blkid -o export /dev/vda2)
 echo "/dev/mapper/cryptswap1 none swap noauto,sw 0 0" >> /etc/fstab
 echo "cryptswap1 UUID=$UUID /dev/urandom noauto,offset=6,swap,cipher=aes-cbc-essiv:sha256" >> /etc/crypttab
+
+# start cryptswap1 manually
+/sbin/cryptdisks_start cryptswap1
+/sbin/swapon /dev/mapper/cryptswap1
+
+# erase traces of firstboot
+mv /etc/rc.local.orig /etc/rc.local
