5998fff6b1ca3453458cac281f00d51c22477e36
Andy Jack <github@veracity.ca>
2015-05-28 17:10:14 -0400

    Clean up fstab entries in post, not firstboot

--- cthost1-post.sh
+++ cthost1-post.sh
@@ -14,4 +14,6 @@ echo "doing execscript.sh: $1"
 cat <<'__EOF__' | chroot "$1" bash -c "cat | bash"
 chown root:root /etc/init/ttyS0.conf /etc/init/cryptswap1.conf
 chmod 644       /etc/init/ttyS0.conf /etc/init/cryptswap1.conf
+/usr/bin/perl -ni -e 'next if m{/dev/sda1}; print' /etc/fstab
+/usr/bin/perl -ni -e 'next if /swap/; print' /etc/fstab
 __EOF__
