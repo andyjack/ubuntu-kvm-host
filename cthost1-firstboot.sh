b5b6b7467257bf58e493ecf023acfb1375919522
Andy Jack <github@veracity.ca>
2015-06-01 10:51:02 -0400

    Don't run kvm inside the vm; dropping vagrant

--- cthost1-firstboot.sh
+++ cthost1-firstboot.sh
@@ -10,13 +10,7 @@ eval $(/sbin/blkid -o export /dev/vda2)
 echo "UUID=$UUID none swap sw 0 0" >> /etc/fstab
 /sbin/swapon -a
 
-### put andy user into group allowed to use kvm vm's
-usermod -G libvirtd -a andy
-
 aptitude update
-
-curl -O -L https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
-dpkg -i vagrant_1.7.2_x86_64.deb
 
 # erase traces of firstboot
 mv /etc/rc.local.orig /etc/rc.local
--- cthost1-post.sh
+++ cthost1-post.sh
@@ -12,7 +12,7 @@ set -e
 echo "doing execscript.sh: $1"
 
 cat <<'__EOF__' | chroot "$1" bash -c "cat | bash"
-for F in /etc/init/ttyS0.conf /etc/apt/apt.conf.d/02proxy /etc/modprobe.d/99-local.conf ; do
+for F in /etc/init/ttyS0.conf /etc/apt/apt.conf.d/02proxy ; do
     chown root:root "$F"
     chown 644       "$F"
 done
